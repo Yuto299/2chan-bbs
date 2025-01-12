fef7de267be26465ef808515d1a7b0cbec0429f3
Yutokara <ota.se.ke@outlook.jp>
2025-01-12 21:10:02 +0900

    xssの対策未実装

--- index.php
+++ index.php
@@ -2,11 +2,33 @@
 
 include_once('./app/database/connect.php');
 
+$pdo->exec("SET NAMES utf8mb4");
+$error_message = array();
+
 if (isset($_POST["submitButton"])) {
-  $username = $_POST["username"];
-  var_dump($username);
-  $body = $_POST["body"];
-  var_dump($body);
+
+  //名前入力チェック
+  if (empty($_POST['username'])) {
+    $error_message['username'] = 'お名前を入力してください';
+  } 
+  //コメント入力チェック
+  if (empty($_POST['body'])) {
+    $error_message['body'] = 'コメントを入力してください';
+  } 
+
+  if (empty($error_message)) {
+    $post_date = date("Y-m-d H:i:s");
+
+    $sql = "INSERT INTO `comment` (`username`, `body`, `post_date`) VALUES (:username, :body, :post_date);";
+    $stmt = $pdo->prepare($sql);
+  
+    //値をセットする
+    $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
+    $stmt->bindParam(':body', $_POST['body'], PDO::PARAM_STR);
+    $stmt->bindParam(':post_date', $post_date, PDO::PARAM_STR);
+  
+    $stmt->execute();
+  }
 }
 
 $comment_array = array();
@@ -17,7 +39,7 @@ $stmt = $pdo->prepare($sql);
 $comment_array = $stmt->execute();
 
 $comment_array = $stmt;
-var_dump($comment_array);
+// var_dump($comment_array);
 
 ?>
 
@@ -35,6 +57,15 @@ var_dump($comment_array);
     <hr>
   </header>
 
+  <!-- バリデーションチェックエラー文表示 -->
+  <?php if (isset($error_message)): ?>
+    <ul class='errorMessage'>
+      <?php foreach ($error_message as $error): ?>
+        <li><?php echo $error ?></li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+
   <!-- スレッドエリア -->
   <div class='threadWrapper'>
     <div class="childWrapper">
@@ -43,16 +74,18 @@ var_dump($comment_array);
         <h1>2ちゃんねる掲示板を作ってみた</h1>
       </div>
       <section>
+        <?php foreach($comment_array as $comment) :?>
         <article>
           <div class="wrapper">
             <div class="nameArea">
               <span>名前:</span>
-              <p class='username'>Yu299</p>
-              <time>:2025/01/12 17:00</time>
+              <p class='username'><?php echo $comment['username']; ?></p>
+              <time>:<?php echo $comment['post_date']; ?></time>
             </div>
-            <p class='comment'>手書きのコメントです。</p>
+            <p class='comment'><?php echo $comment['body']; ?></p>
           </div>
         </article>
+        <?php endforeach; ?>
       </section>
       <form class='formWrapper' method='POST'>
         <div>
