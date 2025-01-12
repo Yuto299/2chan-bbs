e1360bc8c0ba690f644127d164f437fd515c01e1
Yutokara <ota.se.ke@outlook.jp>
2025-01-12 17:07:32 +0900

    掲示板の骨格完了

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
-<?php
-echo 'Hello World';
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2ちゃんねる掲示板</title>
+</head>
+<body>
+  <header>
+    <h1 class='title'>2ちゃんねる掲示板</h1>
+    <hr>
+  </header>
 
-//localhost:8080/2chan-bbs
+  <!-- スレッドエリア -->
+  <div class='threadWrapper'>
+    <div class="childWrapper">
+      <div class='threadTitle'>
+        <span> 【タイトル】 </span>
+        <h1>2ちゃんねる掲示板を作ってみた</h1>
+      </div>
+      <section>
+        <article>
+          <div class="wrapper">
+            <div class="nameArea">
+              <span>名前:</span>
+              <p class='username'>Yu299</p>
+              <time>:2025/01/12 17:00</time>
+            </div>
+            <p class='comment'>手書きのコメントです。</p>
+          </div>
+        </article>
+      </section>
+    </div>
+  </div>
+</body>
+</html>
